10e6a787d5df301b44e759016af82bf1198bf2fc
William Barbosa <heytherewill@gmail.com>
2019-09-18 15:26:12 -0300

    Type erasure killed my crops

--- Toggl.Droid/Adapters/BaseRecyclerAdapter.cs
+++ Toggl.Droid/Adapters/BaseRecyclerAdapter.cs
@@ -1,61 +1,29 @@
-﻿using Android.OS;
-using Android.Runtime;
+﻿using Android.Runtime;
 using Android.Support.V7.Util;
 using Android.Support.V7.Widget;
+using Android.Support.V7.RecyclerView.Extensions;
 using Android.Views;
 using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
-using System.Threading.Tasks;
 using Toggl.Core.UI.Interfaces;
 using Toggl.Droid.Adapters.DiffingStrategies;
 using Toggl.Droid.ViewHolders;
 using Toggl.Shared.Extensions;
-using Handler = Android.OS.Handler;
+using JavaObject = Java.Lang.Object;
 
 namespace Toggl.Droid.Adapters
 {
-    public abstract class BaseRecyclerAdapter<T> : RecyclerView.Adapter
-        where T : IEquatable<T>
+    public abstract class BaseRecyclerAdapter<T> : ListAdapter
+        where T : class, IEquatable<T>
     {
-        private readonly IDiffingStrategy<T> diffingStrategy;
-
         public IObservable<T> ItemTapObservable => itemTapSubject.AsObservable();
 
         private Subject<T> itemTapSubject = new Subject<T>();
 
-        private IImmutableList<T> items = ImmutableList<T>.Empty;
-
-        private IImmutableList<T> nextUpdate;
-
-        private bool isUpdateRunning;
-
-        private readonly object updateLock = new object();
-
-        public virtual IImmutableList<T> Items
-        {
-            get => items;
-            set => SetItems(value ?? ImmutableList<T>.Empty);
-        }
-
         protected BaseRecyclerAdapter(IDiffingStrategy<T> diffingStrategy = null)
+            : base(itemCallbackgFrom(diffingStrategy))
         {
-            this.diffingStrategy = normalizeDiffingStrategy(diffingStrategy);
-
-            HasStableIds = this.diffingStrategy.HasStableIds;
-        }
-
-        private IDiffingStrategy<T> normalizeDiffingStrategy(IDiffingStrategy<T> diffingStrategy)
-        {
-            if (diffingStrategy != null)
-                return diffingStrategy;
-
-            if (typeof(T).ImplementsOrDerivesFrom<IDiffableByIdentifier<T>>())
-                return new IdentifierEqualityDiffingStrategy<T>();
-
-            return new EquatableDiffingStrategy<T>();
         }
 
         protected BaseRecyclerAdapter(IntPtr javaReference, JniHandleOwnership transfer)
@@ -71,102 +39,48 @@ namespace Toggl.Droid.Adapters
             return viewHolder;
         }
 
-        protected abstract BaseRecyclerViewHolder<T> CreateViewHolder(ViewGroup parent, LayoutInflater inflater,
+        protected abstract BaseRecyclerViewHolder<T> CreateViewHolder(
+            ViewGroup parent,
+            LayoutInflater inflater,
             int viewType);
 
         public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
         {
-            var item = GetItem(position);
-            ((BaseRecyclerViewHolder<T>)holder).Item = item;
+            ((BaseRecyclerViewHolder<T>)holder).Item = GetItem(position) as T;
         }
 
-        public override int ItemCount => items.Count;
-
-        public override long GetItemId(int position)
+        private static DiffingStrategyItemCallback<T> itemCallbackgFrom(IDiffingStrategy<T> diffingStrategy)
         {
-            var item = items[position];
-            return diffingStrategy.GetItemId(item);
-        }
-
-        public virtual T GetItem(int viewPosition)
-            => items[viewPosition];
-
-        protected virtual void SetItems(IImmutableList<T> newItems)
-        {
-            lock (updateLock)
+            if (diffingStrategy == null)
             {
-                if (!isUpdateRunning)
+                if (typeof(T).ImplementsOrDerivesFrom<IDiffableByIdentifier<T>>())
                 {
-                    isUpdateRunning = true;
-                    processUpdate(newItems);
+                    diffingStrategy = new IdentifierEqualityDiffingStrategy<T>();
                 }
                 else
                 {
-                    nextUpdate = newItems;
+                    diffingStrategy = new EquatableDiffingStrategy<T>();
                 }
             }
-        }
 
-        private void processUpdate(IImmutableList<T> newItems)
+            return new DiffingStrategyItemCallback<T>(diffingStrategy);
+        }
+    }
+
+    public sealed class DiffingStrategyItemCallback<T> : DiffUtil.ItemCallback
+        where T : class, IEquatable<T>
+    {
+        private readonly IDiffingStrategy<T> diffingStrategy;
+
+        public DiffingStrategyItemCallback(IDiffingStrategy<T> diffingStrategy)
         {
-            var oldItems = items;
-            var handler = new Handler(Looper.MainLooper);
-            Task.Run(() =>
-            {
-                var diffResult = DiffUtil.CalculateDiff(new BaseDiffCallBack(oldItems, newItems, diffingStrategy));
-                handler.Post(() => dispatchUpdates(newItems, diffResult));
-            });
+            this.diffingStrategy = diffingStrategy;
         }
 
-        private void dispatchUpdates(IImmutableList<T> newItems, DiffUtil.DiffResult diffResult)
-        {
-            items = newItems;
-            diffResult.DispatchUpdatesTo(this);
-            lock (updateLock)
-            {
-                if (nextUpdate != null)
-                {
-                    processUpdate(nextUpdate);
-                    nextUpdate = null;
-                }
-                else
-                {
-                    isUpdateRunning = false;
-                }
-            }
-        }
+        public override bool AreContentsTheSame(JavaObject oldItem, JavaObject newItem)
+            => diffingStrategy.AreContentsTheSame(oldItem as T, newItem as T);
 
-        private sealed class BaseDiffCallBack : DiffUtil.Callback
-        {
-            private IImmutableList<T> oldItems;
-            private IImmutableList<T> newItems;
-            private IDiffingStrategy<T> diffingStrategy;
-
-            public BaseDiffCallBack(IImmutableList<T> oldItems, IImmutableList<T> newItems, IDiffingStrategy<T> diffingStrategy)
-            {
-                this.oldItems = oldItems;
-                this.newItems = newItems;
-                this.diffingStrategy = diffingStrategy;
-            }
-
-            public override bool AreContentsTheSame(int oldItemPosition, int newItemPosition)
-            {
-                var oldItem = oldItems[oldItemPosition];
-                var newItem = newItems[newItemPosition];
-
-                return diffingStrategy.AreContentsTheSame(oldItem, newItem);
-            }
-
-            public override bool AreItemsTheSame(int oldItemPosition, int newItemPosition)
-            {
-                var oldItem = oldItems[oldItemPosition];
-                var newItem = newItems[newItemPosition];
-
-                return diffingStrategy.AreItemsTheSame(oldItem, newItem);
-            }
-
-            public override int NewListSize => newItems.Count;
-            public override int OldListSize => oldItems.Count;
-        }
+        public override bool AreItemsTheSame(JavaObject oldItem, JavaObject newItem)
+            => diffingStrategy.AreItemsTheSame(oldItem as T, newItem as T);
     }
 }
